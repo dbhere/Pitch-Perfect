75ce057a28974b2a63c2ce667a01a2fa90de97cb
alexscottzs <alexzhangshuo@gmail.com>
2015-12-20 22:30:49 +0800

    style:隐藏stopbutton

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,19 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
     }
+    
 
     @IBAction func recordAudio(sender: UIButton) {
         //add text "正在录音"
         recordingInProgress.hidden = false
+        stopButton.hidden = false
         //TODO: 录音
         print("I'm recording")
     
