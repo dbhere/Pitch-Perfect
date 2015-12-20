7c31891a2d4ca08309143d31f4da6348a1050498
alexscottzs <alexzhangshuo@gmail.com>
2015-12-20 23:11:23 +0800

    feat:add PlaySoundsViewController

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by HhhotDog on 15/12/20.
+//  Copyright © 2015年 Alexscott. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
