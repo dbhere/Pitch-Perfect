02639582205fb5d5ab7c1f0e28c913d109558201
alexscottzs <alexzhangshuo@gmail.com>
2015-12-21 10:41:59 +0800

    增加model->RecordedAudio用来记录保存的音频url和title

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by HhhotDog on 15/12/21.
+//  Copyright © 2015年 Alexscott. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio:NSObject {
+    var filePathUrl:NSURL!
+    var title:String!
+}
